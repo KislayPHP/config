7717a33e5a56f25471850625a720e1296442b569
kislayraj <kislayraj.89@gmail.com>
2026-02-09 08:34:10 +0530

    Update module sources and examples

--- example.php
+++ example.php
@@ -5,6 +5,28 @@
 extension_loaded('kislayphp_config') or die('kislayphp_config not loaded');
 
 $cfg = new KislayPHP\Config\ConfigClient();
+
+class ArrayConfigClient implements KislayPHP\Config\ClientInterface {
+	private array $data = [];
+
+	public function set(string $key, string $value): bool {
+		$this->data[$key] = $value;
+		return true;
+	}
+
+	public function get(string $key, mixed $default = null): mixed {
+		return $this->data[$key] ?? $default;
+	}
+
+	public function all(): array {
+		return $this->data;
+	}
+}
+
+$use_client = false;
+if ($use_client) {
+	$cfg->setClient(new ArrayConfigClient());
+}
 $cfg->set('db.host', '127.0.0.1');
 $cfg->set('db.port', '5432');
 
--- kislayphp_config.cpp
+++ kislayphp_config.cpp
@@ -1,19 +1,69 @@
 extern "C" {
 #include "php.h"
 #include "ext/standard/info.h"
+#include "Zend/zend_API.h"
+#include "Zend/zend_interfaces.h"
 #include "Zend/zend_exceptions.h"
 }
 
 #include "php_kislayphp_config.h"
 
+#include <cstring>
 #include <string>
 #include <unordered_map>
 
+#ifndef zend_call_method_with_0_params
+static inline void kislayphp_call_method_with_0_params(
+    zend_object *obj,
+    zend_class_entry *obj_ce,
+    zend_function **fn_proxy,
+    const char *function_name,
+    zval *retval) {
+    zend_call_method(obj, obj_ce, fn_proxy, function_name, std::strlen(function_name), retval, 0, nullptr, nullptr);
+}
+
+#define zend_call_method_with_0_params(obj, obj_ce, fn_proxy, function_name, retval) \
+    kislayphp_call_method_with_0_params(obj, obj_ce, fn_proxy, function_name, retval)
+#endif
+
+#ifndef zend_call_method_with_1_params
+static inline void kislayphp_call_method_with_1_params(
+    zend_object *obj,
+    zend_class_entry *obj_ce,
+    zend_function **fn_proxy,
+    const char *function_name,
+    zval *retval,
+    zval *param1) {
+    zend_call_method(obj, obj_ce, fn_proxy, function_name, std::strlen(function_name), retval, 1, param1, nullptr);
+}
+
+#define zend_call_method_with_1_params(obj, obj_ce, fn_proxy, function_name, retval, param1) \
+    kislayphp_call_method_with_1_params(obj, obj_ce, fn_proxy, function_name, retval, param1)
+#endif
+
+#ifndef zend_call_method_with_2_params
+static inline void kislayphp_call_method_with_2_params(
+    zend_object *obj,
+    zend_class_entry *obj_ce,
+    zend_function **fn_proxy,
+    const char *function_name,
+    zval *retval,
+    zval *param1,
+    zval *param2) {
+    zend_call_method(obj, obj_ce, fn_proxy, function_name, std::strlen(function_name), retval, 2, param1, param2);
+}
+
+#define zend_call_method_with_2_params(obj, obj_ce, fn_proxy, function_name, retval, param1, param2) \
+    kislayphp_call_method_with_2_params(obj, obj_ce, fn_proxy, function_name, retval, param1, param2)
+#endif
 static zend_class_entry *kislayphp_config_ce;
+static zend_class_entry *kislayphp_config_client_ce;
 
 typedef struct _php_kislayphp_config_t {
     zend_object std;
     std::unordered_map<std::string, std::string> values;
+    zval client;
+    bool has_client;
 } php_kislayphp_config_t;
 
 static zend_object_handlers kislayphp_config_handlers;
@@ -29,12 +79,17 @@ static zend_object *kislayphp_config_create_object(zend_class_entry *ce) {
     zend_object_std_init(&obj->std, ce);
     object_properties_init(&obj->std, ce);
     new (&obj->values) std::unordered_map<std::string, std::string>();
+    ZVAL_UNDEF(&obj->client);
+    obj->has_client = false;
     obj->std.handlers = &kislayphp_config_handlers;
     return &obj->std;
 }
 
 static void kislayphp_config_free_obj(zend_object *object) {
     php_kislayphp_config_t *obj = php_kislayphp_config_from_obj(object);
+    if (obj->has_client) {
+        zval_ptr_dtor(&obj->client);
+    }
     obj->values.~unordered_map();
     zend_object_std_dtor(&obj->std);
 }
@@ -52,8 +107,38 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_kislayphp_config_get, 0, 0, 1)
     ZEND_ARG_INFO(0, default)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_kislayphp_config_set_client, 0, 0, 1)
+    ZEND_ARG_OBJ_INFO(0, client, KislayPHP\\Config\\ClientInterface, 0)
+ZEND_END_ARG_INFO()
+
 PHP_METHOD(KislayPHPConfig, __construct) {
     ZEND_PARSE_PARAMETERS_NONE();
+}
+
+PHP_METHOD(KislayPHPConfig, setClient) {
+    zval *client = nullptr;
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_ZVAL(client)
+    ZEND_PARSE_PARAMETERS_END();
+
+    if (client == nullptr || Z_TYPE_P(client) != IS_OBJECT) {
+        zend_throw_exception(zend_ce_exception, "Client must be an object", 0);
+        RETURN_FALSE;
+    }
+
+    if (!instanceof_function(Z_OBJCE_P(client), kislayphp_config_client_ce)) {
+        zend_throw_exception(zend_ce_exception, "Client must implement KislayPHP\\Config\\ClientInterface", 0);
+        RETURN_FALSE;
+    }
+
+    php_kislayphp_config_t *obj = php_kislayphp_config_from_obj(Z_OBJ_P(getThis()));
+    if (obj->has_client) {
+        zval_ptr_dtor(&obj->client);
+        obj->has_client = false;
+    }
+    ZVAL_COPY(&obj->client, client);
+    obj->has_client = true;
+    RETURN_TRUE;
 }
 
 PHP_METHOD(KislayPHPConfig, set) {
@@ -67,6 +152,26 @@ PHP_METHOD(KislayPHPConfig, set) {
     ZEND_PARSE_PARAMETERS_END();
 
     php_kislayphp_config_t *obj = php_kislayphp_config_from_obj(Z_OBJ_P(getThis()));
+    if (obj->has_client) {
+        zval key_zv;
+        zval value_zv;
+        ZVAL_STRINGL(&key_zv, key, key_len);
+        ZVAL_STRINGL(&value_zv, value, value_len);
+
+        zval retval;
+        ZVAL_UNDEF(&retval);
+        zend_call_method_with_2_params(Z_OBJ(obj->client), Z_OBJCE(obj->client), nullptr, "set", &retval, &key_zv, &value_zv);
+
+        zval_ptr_dtor(&key_zv);
+        zval_ptr_dtor(&value_zv);
+
+        if (Z_ISUNDEF(retval)) {
+            RETURN_TRUE;
+        }
+        RETVAL_ZVAL(&retval, 1, 1);
+        return;
+    }
+
     obj->values[std::string(key, key_len)] = std::string(value, value_len);
     RETURN_TRUE;
 }
@@ -82,6 +187,26 @@ PHP_METHOD(KislayPHPConfig, get) {
     ZEND_PARSE_PARAMETERS_END();
 
     php_kislayphp_config_t *obj = php_kislayphp_config_from_obj(Z_OBJ_P(getThis()));
+    if (obj->has_client) {
+        zval key_zv;
+        ZVAL_STRINGL(&key_zv, key, key_len);
+
+        zval retval;
+        ZVAL_UNDEF(&retval);
+        if (default_val != nullptr) {
+            zend_call_method_with_2_params(Z_OBJ(obj->client), Z_OBJCE(obj->client), nullptr, "get", &retval, &key_zv, default_val);
+        } else {
+            zend_call_method_with_1_params(Z_OBJ(obj->client), Z_OBJCE(obj->client), nullptr, "get", &retval, &key_zv);
+        }
+        zval_ptr_dtor(&key_zv);
+
+        if (Z_ISUNDEF(retval)) {
+            RETURN_NULL();
+        }
+        RETVAL_ZVAL(&retval, 1, 1);
+        return;
+    }
+
     auto it = obj->values.find(std::string(key, key_len));
     if (it == obj->values.end()) {
         if (default_val != nullptr) {
@@ -94,6 +219,19 @@ PHP_METHOD(KislayPHPConfig, get) {
 
 PHP_METHOD(KislayPHPConfig, all) {
     php_kislayphp_config_t *obj = php_kislayphp_config_from_obj(Z_OBJ_P(getThis()));
+    if (obj->has_client) {
+        zval retval;
+        ZVAL_UNDEF(&retval);
+        zend_call_method_with_0_params(Z_OBJ(obj->client), Z_OBJCE(obj->client), nullptr, "all", &retval);
+
+        if (Z_ISUNDEF(retval)) {
+            array_init(return_value);
+            return;
+        }
+        RETVAL_ZVAL(&retval, 1, 1);
+        return;
+    }
+
     array_init(return_value);
     for (const auto &entry : obj->values) {
         add_assoc_string(return_value, entry.first.c_str(), entry.second.c_str());
@@ -102,14 +240,24 @@ PHP_METHOD(KislayPHPConfig, all) {
 
 static const zend_function_entry kislayphp_config_methods[] = {
     PHP_ME(KislayPHPConfig, __construct, arginfo_kislayphp_config_void, ZEND_ACC_PUBLIC)
+    PHP_ME(KislayPHPConfig, setClient, arginfo_kislayphp_config_set_client, ZEND_ACC_PUBLIC)
     PHP_ME(KislayPHPConfig, set, arginfo_kislayphp_config_set, ZEND_ACC_PUBLIC)
     PHP_ME(KislayPHPConfig, get, arginfo_kislayphp_config_get, ZEND_ACC_PUBLIC)
     PHP_ME(KislayPHPConfig, all, arginfo_kislayphp_config_void, ZEND_ACC_PUBLIC)
     PHP_FE_END
 };
 
+static const zend_function_entry kislayphp_config_client_methods[] = {
+    ZEND_ABSTRACT_ME(KislayPHPConfigClientInterface, set, arginfo_kislayphp_config_set)
+    ZEND_ABSTRACT_ME(KislayPHPConfigClientInterface, get, arginfo_kislayphp_config_get)
+    ZEND_ABSTRACT_ME(KislayPHPConfigClientInterface, all, arginfo_kislayphp_config_void)
+    PHP_FE_END
+};
+
 PHP_MINIT_FUNCTION(kislayphp_config) {
     zend_class_entry ce;
+    INIT_NS_CLASS_ENTRY(ce, "KislayPHP\\Config", "ClientInterface", kislayphp_config_client_methods);
+    kislayphp_config_client_ce = zend_register_internal_interface(&ce);
     INIT_NS_CLASS_ENTRY(ce, "KislayPHP\\Config", "ConfigClient", kislayphp_config_methods);
     kislayphp_config_ce = zend_register_internal_class(&ce);
     kislayphp_config_ce->create_object = kislayphp_config_create_object;
